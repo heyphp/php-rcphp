c346a6b7794e6085c7b393af274e175894014ec0
daxiong123 <zhangwj@2345.com>
2014-07-14 23:08:51 +0800

    Code format.

--- rcphp/Util/Check.class.php
+++ rcphp/Util/Check.class.php
@@ -1,9 +1,8 @@
 <?php
 /**
  * Check class file.
-
  *
-*@author         RcPHP Dev Team
+ * @author         RcPHP Dev Team
  * @copyright      Copyright (c) 2013,RcPHP Dev Team
  * @license        Apache License 2.0 {@link http://www.apache.org/licenses/LICENSE-2.0}
  * @package        Util
