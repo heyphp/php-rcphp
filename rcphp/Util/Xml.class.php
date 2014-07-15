ea3b6a1eb64dfadc234c08b82ded80ec130ab705
daxiong123 <zhangwj@2345.com>
2014-07-15 17:46:13 +0800

    Fix code comment

--- rcphp/Util/Xml.class.php
+++ rcphp/Util/Xml.class.php
@@ -2,7 +2,7 @@
 /**
  * XML class php
  *
-*@author         RcPHP Dev Team
+ * @author         RcPHP Dev Team
  * @copyright      Copyright (c) 2013,RcPHP Dev Team
  * @license        Apache License 2.0 {@link http://www.apache.org/licenses/LICENSE-2.0}
  * @package        Library.Util
