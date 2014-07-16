5d6df0186172100db15064cac52355dfd97875b7
daxiong123 <zhangwj@2345.com>
2014-07-16 14:57:01 +0800

    Fix code comment

--- rcphp/Net/Upload.class.php
+++ rcphp/Net/Upload.class.php
@@ -1,9 +1,8 @@
 <?php
 /**
  * Upload class file.
-
  *
-*@author         RcPHP Dev Team
+ * @author         RcPHP Dev Team
  * @copyright      Copyright (c) 2013,RcPHP Dev Team
  * @license        Apache License 2.0 {@link http://www.apache.org/licenses/LICENSE-2.0}
  * @package        Net
