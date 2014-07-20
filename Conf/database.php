af5da2c73971b0a74d126fa5060a383973caf7d3
daxiong123 <zhangwj@2345.com>
2014-07-20 21:41:55 +0800

    Update

---
+++ Conf/database.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangwj
+ * Date: 14-7-20
+ * Time: ÏÂÎç9:07
+ */
+
+if($_ENV['VCAP_SERVICES'])
+{
+	$conf = json_decode($_ENV['VCAP_SERVICES'], true);
+
+	return array(
+		'driver' => 'mysql',
+		'charset' => 'GBK',
+		'prefix' => 'rcphp_',
+		'master' => array(
+			'host' => $conf['mysql']['credentials']['hostname'],
+			'port' => $conf['mysql']['credentials']['port'],
+			'user' => $conf['mysql']['credentials']['username'],
+			'password' => $conf['mysql']['credentials']['password'],
+			'database' => $conf['mysql']['credentials']['name']
+		),
+	);
+}
+else
+{
+	return array(
+		'driver' => 'mysql',
+		'charset' => 'GBK',
+		'prefix' => 'rcphp_',
+		'master' => array(
+			'host' => '127.0.0.1',
+			'port' => 3306,
+			'user' => 'root',
+			'password' => '',
+			'database' => 'blog'
+		),
+	);
+}
