7f11419e51f96de7a242bb178f68857c17c801f7
daxiong123 <zhangwj@2345.com>
2014-07-20 18:23:39 +0800

    Demo.

---
+++ Application/Controller/indexController.class.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * indexController.class.php
+ * Ä¬ÈÏ¿ØÖÆÆ÷
+ *
+ * @author         RcPHP Dev Team
+ * @copyright      Copyright (c) 2013,RcPHP Dev Team
+ * @license        Apache License 2.0 {@link http://www.apache.org/licenses/LICENSE-2.0}
+ * @filesource
+ */
+defined('IN_RCPHP') or exit('Access denied');
+
+class indexController extends Controller
+{
+
+	public function index()
+	{
+		$this->assign(array())
+			 ->display();
+	}
+}
