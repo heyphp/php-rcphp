d100de98ac6f1167a817b7aeed69253f2ffbb8e3
daxiong123 <zhangwj@2345.com>
2014-07-27 20:50:45 +0800

    Oauth classes.

---
+++ RcPHP/Oauth/Wangyi.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Wangyi class file.
+ *
+ * @author         RcPHP Dev Team
+ * @copyright      Copyright (c) 2013,RcPHP Dev Team
+ * @license        Apache License 2.0 {@link http://www.apache.org/licenses/LICENSE-2.0}
+ * @package        Oauth
+ * @since          1.0
+ */
+defined('IN_RCPHP') or exit('Access denied');
+
+class Wangyi
+{
+	
+}
