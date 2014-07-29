fbfc90a5a6aeb2a5997daed4b46781e4e3891395
daxiong123 <zhangwj@2345.com>
2014-07-29 19:03:55 +0800

    Commit

--- RcPHP/Oauth/Baidu.class.php
+++ RcPHP/Oauth/Baidu.class.php
@@ -12,5 +12,53 @@ defined('IN_RCPHP') or exit('Access denied');
 
 class Baidu
 {
-	
+
+	/**
+	 * 百度帐号体系 Api地址
+	 *
+	 * @var string
+	 */
+	private $api_url = 'https://openapi.baidu.com/rest/2.0/';
+
+	/**
+	 * Client ID.
+	 *
+	 * @var null|string
+	 */
+	private $client_id = null;
+
+	/**
+	 * Client Secret.
+	 *
+	 * @var null|string
+	 */
+	private $client_secret = null;
+
+	/**
+	 * Access token.
+	 *
+	 * @var null|string
+	 */
+	private $access_token = null;
+
+	/**
+	 * 构造方法 百度帐号配置
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$conf = RcPHP::getConfig("baidu");
+
+		if(!empty($conf) && !empty($conf['client_id']) && !empty($conf['client_secret']))
+		{
+			$this->client_id = $conf['client_id'];
+			$this->client_secret = $conf['client_secret'];
+			empty($conf['access_token']) or $this->access_token = $conf['access_token'];
+		}
+		else
+		{
+			Controller::halt('Baidu configuration error.');
+		}
+	}
 }
