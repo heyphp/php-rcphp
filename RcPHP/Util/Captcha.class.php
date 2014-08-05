a86013870df9380181f41e67ab84d9cbd6a761db
daxiong123 <zhangwj@2345.com>
2014-08-05 21:09:59 +0800

    Update

--- RcPHP/Util/Captcha.class.php
+++ RcPHP/Util/Captcha.class.php
@@ -5,7 +5,7 @@
  * @author         RcPHP Dev Team
  * @copyright      Copyright (c) 2013,RcPHP Dev Team
  * @license        Apache License 2.0 {@link http://www.apache.org/licenses/LICENSE-2.0}
- * @package        Library.Util
+ * @package        Util
  * @since          1.0
  */
 defined('IN_RCPHP') or exit('Access denied');
--- RcPHP/Util/File.class.php
+++ RcPHP/Util/File.class.php
@@ -6,7 +6,7 @@
  * @version        $Id: File.class.php 0.2 2013-08-14 23:05 zhangwj $
  * @copyright      Copyright (c) 2013,RcPHP Dev Team
  * @license        Apache License 2.0 {@link http://www.apache.org/licenses/LICENSE-2.0}
- * @package        Library.Util
+ * @package        Util
  * @since          1.0
  */
 defined('IN_RCPHP') or exit('Access denied');
