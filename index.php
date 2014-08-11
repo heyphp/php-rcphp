c3b6d9cc1720e2de243a9b0f566d82b42025c434
daxiong123 <zhangwj@2345.com>
2014-08-12 00:20:26 +0800

    Update

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
  *
  * @author        zhangwj<zhangwj@2345.com>
  * @copyright     Copyright (c) 2013,RcPHP Dev Team
- * @filesource
  */
+header("Content-Type:text/html;charset=GBK");
+
 define('IN_RCPHP', true);
 
 /**
@@ -16,12 +17,22 @@ define("APP_PATH", dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Application' . DIR
 /**
  * 定义项目使用编码
  */
-define('CHARSET', 'UTF-8');
+define('CHARSET', 'GBK');
+
+/**
+ * 路由模式
+ */
+define('URL_MODEL', 2);
 
 /**
  * 定义项目是否开启debug模式
  */
 define('APP_DEBUG', true);
+
+/**
+ * 应用版本号
+ */
+define('APP_VERSION', 0.1);
 
 /**
  * 引入框架主文件
