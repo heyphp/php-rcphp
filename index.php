f140d15fc02f3a86d9bdd98fcf45464876b069ef
daxiong123 <zhangwj@2345.com>
2014-08-17 17:14:30 +0800

    Fix bug

--- index.php
+++ index.php
@@ -2,31 +2,38 @@
 /**
  * Bootstrap.
  *
- * @author zhangwj<zhangwj@2345.com>
+ * @author    zhangwj<zhangwj@2345.com>
  * @copyright Copyright (c) 2013,RcPHP Dev Team
  */
 header("Content-Type:text/html;charset=GBK");
+
 define('IN_RCPHP', true);
+
 /**
  * 定义项目所在路径
  */
 define("APP_PATH", dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Application' . DIRECTORY_SEPARATOR);
+
 /**
  * 定义项目使用编码
  */
 define('CHARSET', 'GBK');
+
 /**
  * 路由模式
  */
 define('URL_MODEL', 2);
+
 /**
  * 定义项目是否开启debug模式
  */
 define('APP_DEBUG', true);
+
 /**
  * 应用版本号
  */
 define('APP_VERSION', 0.1);
+
 /**
  * 引入框架主文件
  */
