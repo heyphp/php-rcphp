fc2e405394d9c5386664c7076b3a67a6c9ec423b
daxiong123 <zhangwj@2345.com>
2014-09-06 13:56:21 +0800

    Update

--- Application/View/Index/index.php
+++
@@ -1,6 +0,0 @@
-<?php $this->layout("header");?>
-<div class="container clearfix row mt51">
-	<div class="col-sm-8" style="height: 500px;background-color: red"></div>
-	<div class="col-sm-4" style="height: 500px;background-color: gray"></div>
-</div>
-<?php $this->layout("footer");?>
