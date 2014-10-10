3960e01e32ee38ccde26248a9a508d76cb859aff
daxiong123 <zhangwj@2345.com>
2014-10-10 09:21:22 +0800

    Modify Log class.

--- RcPHP/Core/Log.class.php
+++ RcPHP/Core/Log.class.php
@@ -15,6 +15,15 @@ defined('IN_RCPHP') or exit('Access denied');
 class Log
 {
 
+	const FATAL = 'FATAL';  // 致命错误: 导致系统崩溃无法使用
+	const ERR = 'ERR';  // 一般错误: 一般性错误
+	const WARN = 'WARN';  // 警告性错误: 需要发出警告的错误
+	const DEPRECATED = 'DEPRECATED'; // 不推荐用法：旧方法
+	const NOTICE = 'NOTIC';  // 通知: 程序可以运行但是还不够完美的错误
+	const INFO = 'INFO';  // 信息: 程序输出信息
+	const DEBUG = 'DEBUG';  // 调试: 调试信息
+	const SQL = 'SQL';  // SQL：SQL语句 注意只在调试模式开启时有效
+
 	/**
 	 * Write log file.
 	 *
@@ -23,36 +32,26 @@ class Log
 	 * @param string $logFileName
 	 * @return bool
 	 */
-	public static function write($message, $sourceFile, $level = 'Error', $fileName = null)
+	public static function write($message, $level = self::ERR, $fileName = null)
 	{
-		if(empty($message))
+		if(defined('RCPHP_LOG') || RCPHP_LOG == 1)
 		{
-			return false;
+			$fileName = self::getLogFile($fileName);
+
+			$logDir = dirname($fileName);
+
+			if(!is_dir($logDir))
+			{
+				mkdir($logDir, 0777, true);
+			}
+
+			if(!is_writable($logDir))
+			{
+				chmod($logDir, 0777);
+			}
+
+			error_log(date('[Y-m-d H:i:s]') . " [" . $level . "] [client " . $_SERVER['REMOTE_ADDR'] . "] " . $message . ": " . $_SERVER['REQUEST_URI'] . "\r\n", 3, $fileName);
 		}
-
-		//Status
-		if(!defined('RCPHP_LOG') || (defined('RCPHP_LOG') && RCPHP_LOG == 0))
-		{
-			return true;
-		}
-
-		$fileName = self::getLogFile($fileName);
-
-		$logDir = dirname($fileName);
-
-		if(!is_dir($logDir))
-		{
-			mkdir($logDir, 0777, true);
-		}
-
-		if(!is_writable($logDir))
-		{
-			chmod($logDir, 0777);
-		}
-
-		error_log(date('[Y-m-d H:i:s]', time()) . " [" . $level . "] [client " . (function_exists('getIp') ? getIp() : '127.0.0.1') . "] " . $message . ": " . $sourceFile . "\r\n", 3, $fileName);
-
-		return true;
 	}
 
 	/**
