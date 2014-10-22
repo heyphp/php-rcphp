51b791b84cbe0cf5678874d70a5e8868e3ceac9c
daxiong123 <zhangwj@2345.com>
2014-10-22 23:49:11 +0800

    Update weibo oauth class.

--- RcPHP/Oauth/Weibo.class.php
+++ RcPHP/Oauth/Weibo.class.php
@@ -8,6 +8,8 @@
  * @package        Oauth
  * @since          1.0
  */
+namespace RCPHP\Oauth;
+
 defined('IN_RCPHP') or exit('Access denied');
 
 class Weibo extends Oauth
@@ -18,106 +20,57 @@ class Weibo extends Oauth
 	 *
 	 * @var string
 	 */
-	private $apiBase = 'https://api.weibo.com/2/';
+	protected $apiBase = 'https://api.weibo.com/2/';
 
 	/**
-	 * 构造方法 Weibo配置
+	 * 微博 登录地址
 	 *
-	 * @return void
+	 * @var string
 	 */
-	public function __construct()
+	protected $loginUrl = 'https://api.weibo.com/oauth2/authorize';
+
+	/**
+	 * 微博 Token地址
+	 *
+	 * @var string
+	 */
+	protected $accessTokenUrl = 'https://api.weibo.com/oauth2/access_token';
+
+	/**
+	 * 解析access_token
+	 *
+	 * @param array $data
+	 * @return mixed|void
+	 * @throws \Exception
+	 */
+	protected function parseToken(array $data)
 	{
-		parent::__construct();
-
-		$conf = RcPHP::getConfig("weibo");
-
-		if(!empty($conf) && !empty($conf['appKey']) && !empty($conf['appSecret']) && !empty($conf['callback']))
+		if(empty($data))
 		{
-			$this->appKey = $conf['appKey'];
-			$this->appSecret = $conf['appSecret'];
-			$this->callback = $conf['callback'];
+			throw new \Exception("Param data is null.");
+		}
+
+		$data = json_decode($data, true);
+
+		if(!empty($data['access_token']) && !empty($data['expires_in']))
+		{
+			$this->token = $data;
+
+			$data['openid'] = $this->getOpenId();
+
+			return $data;
 		}
 		else
 		{
-			Controller::halt('Weibo configuration error.');
+			throw new \Exception("Get access token is error,error message is " . $data['error']);
 		}
-	}
-
-	/**
-	 * 生成授权网址
-	 *
-	 * @param string $callback_url
-	 * @param string $scope
-	 * @return string
-	 */
-	public function login_url($callback_url = '', $scope = '', $display = 'default')
-	{
-		if(!empty($callback))
-		{
-			$this->callback = $callback;
-		}
-
-		$params = array(
-			'client_id' => $this->appKey,
-			'redirect_uri' => $this->callback,
-			'scope' => $scope,
-			'state' => md5(time()),
-			'display' => $display
-		);
-
-		return 'https://api.weibo.com/oauth2/authorize?' . http_build_query($params);
-	}
-
-	/**
-	 * 获取access_token
-	 *
-	 * @param string $code
-	 * @return array
-	 */
-	public function access_token($code)
-	{
-		if(empty($code))
-		{
-			Controller::halt("The code param is null.");
-		}
-
-		$params = array(
-			'grant_type' => $this->grantType,
-			'code' => $code,
-			'client_id' => $this->appKey,
-			'client_secret' => $this->appSecret,
-			'redirect_uri' => $this->callback
-		);
-		$url = 'https://api.weibo.com/oauth2/access_token';
-
-		$result = $this->http($url, http_build_query($params), 'POST');
-
-		if(!empty($result))
-		{
-			$this->token = $this->parseToken($result);
-
-			return $this->token;
-		}
-		else
-		{
-			return array();
-		}
-	}
-
-	/**
-	 * 获取授权用户信息
-	 *
-	 * @return string
-	 */
-	public function me()
-	{
-		return $this->http("https://api.weibo.com/oauth2/get_token_info", array(), "POST");
 	}
 
 	/**
 	 * 获取授权用户ID
 	 *
-	 * @return mixed|null
+	 * @return mixed|void
+	 * @throws \Exception
 	 */
 	public function getOpenId()
 	{
@@ -126,17 +79,15 @@ class Weibo extends Oauth
 			return $this->token['openid'];
 		}
 
-		$data = $this->me();
+		$data = $this->api("account/get_uid");
 
-		if(!empty($data))
+		if(!empty($data) && !empty($data['uid']))
 		{
-			$data = json_decode($data, true);
-
-			return empty($data['uid']) ? null : $data['uid'];
+			return (int)$data['uid'];
 		}
 		else
 		{
-			return null;
+			throw new \Exception("Get Open ID is error,error message is " . $data['error']);
 		}
 	}
 
@@ -152,20 +103,21 @@ class Weibo extends Oauth
 	{
 		if(empty($url))
 		{
-			Controller::halt("The url param is null.");
+			\RCPHP\Controller::halt("The url param is null.");
 		}
 
 		$url = $this->$apiBase . $url;
-		if(!empty($this->token['token']))
+
+		$params['access_token'] = $this->token['access_token'];
+
+		if($method == 'GET')
 		{
-			$params['access_token'] = $this->token['token'];
+			$result = $this->http($url . '?' . http_build_query($params));
 		}
 		else
 		{
-			throw new Exception("The access token is null.");
+			$result = $this->http($url, http_build_query($params), 'POST');
 		}
-
-		$result = $this->http($url, $params, $method);
 
 		if(!empty($result))
 		{
