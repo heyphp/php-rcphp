5ea104fecf4c07c66b1a6a5adf0eaac42379c217
daxiong123 <zhangwj@2345.com>
2014-10-30 20:47:18 +0800

    Update oauth class.

--- RcPHP/Oauth/Wangyi.class.php
+++ RcPHP/Oauth/Wangyi.class.php
@@ -8,9 +8,123 @@
  * @package        Oauth
  * @since          1.0
  */
+namespace RCPHP\Oauth;
+
 defined('IN_RCPHP') or exit('Access denied');
 
-class Wangyi
+class Wangyi extends \RCPHP\Oauth\Oauth
 {
-	
+
+	/**
+	 * 网易帐号体系 Api地址
+	 *
+	 * @var string
+	 */
+	private $apiBase = 'https://api.t.163.com/';
+
+	/**
+	 * 网易 登录地址
+	 *
+	 * @var string
+	 */
+	protected $loginUrl = 'https://api.t.163.com/oauth2/authorize';
+
+	/**
+	 * 网易 Token地址
+	 *
+	 * @var string
+	 */
+	protected $accessTokenUrl = 'https://api.t.163.com/oauth2/access_token';
+
+	/**
+	 * 解析access_token
+	 *
+	 * @param array $data
+	 * @return array
+	 * @throws \Exception
+	 */
+	protected function parseToken(array $data)
+	{
+		if(empty($data))
+		{
+			throw new \Exception("Param data is null.");
+		}
+
+		$data = json_decode($data, true);
+
+		if($data['oauth_token'] && $data['oauth_token_secreate'])
+		{
+			$this->token = $data;
+
+			$data['openid'] = $this->getOpenId();
+
+			return $data;
+		}
+		else
+		{
+			throw new \Exception("Get access token is error,error message is " . $data['error']);
+		}
+	}
+
+	/**
+	 * 获取登录用户ID
+	 *
+	 * @return null|string
+	 */
+	public function getOpenId()
+	{
+		if(!empty($this->token['openid']))
+		{
+			return $this->token['openid'];
+		}
+
+		$data = $this->api('users/show.json');
+
+		if(!empty($data) && !empty($data['id']))
+		{
+			return $data['id'];
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	/**
+	 * 其他Api调用
+	 *
+	 * @param string $url
+	 * @param array  $params
+	 * @param string $method
+	 * @return array|mixed
+	 */
+	public function api($url, array $params = array(), $method = 'GET')
+	{
+		if(empty($url))
+		{
+			\RCPHP\Controller::halt("The url param is null.");
+		}
+
+		$url = $this->$apiBase . $url;
+
+		$params['access_token'] = $this->token['access_token'];
+
+		if($method == 'GET')
+		{
+			$result = $this->http($url . '?' . http_build_query($params));
+		}
+		else
+		{
+			$result = $this->http($url, http_build_query($params), 'POST');
+		}
+
+		if(!empty($result))
+		{
+			return json_decode($result, true);
+		}
+		else
+		{
+			return array();
+		}
+	}
 }
