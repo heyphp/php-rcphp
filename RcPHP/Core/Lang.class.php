bea1614afca3a01c78004a0f9b68c22f82013e76
daxiong123 <zhangwj@2345.com>
2014-11-18 19:40:18 +0800

    Add lang class file.

---
+++ RcPHP/Core/Lang.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Lang class file.
+ *
+ * @author         RcPHP Dev Team
+ * @copyright      Copyright (c) 2013,RcPHP Dev Team
+ * @license        Apache License 2.0 {@link http://www.apache.org/licenses/LICENSE-2.0}
+ * @package        Core
+ * @since          1.0
+ */
+namespace RCPHP;
+
+defined('IN_RCPHP') or exit('Access denied');
+
+class Lang
+{
+	
+}
